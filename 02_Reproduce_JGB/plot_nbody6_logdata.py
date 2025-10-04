c0732a7761943cba47ff3d908a01fc676e55c7f8
Yana Savchenko <savchenkoyana97@gmail.com>
2025-10-04 18:59:28 +0300

    fix print pandas format

--- 02_Reproduce_JGB/plot_nbody6_logdata.py
+++ 02_Reproduce_JGB/plot_nbody6_logdata.py
@@ -18,7 +18,6 @@ def setup_pandas():
     pd.set_option("display.max_rows", None)
     pd.set_option("display.max_columns", None)
     pd.set_option("display.width", 2000)
-    pd.set_option("display.float_format", "{:20,.2f}".format)
     pd.set_option("display.max_colwidth", None)
 
 
